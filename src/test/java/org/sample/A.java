8df77d3d8fe3eb873b67dd0b54473ca15ac432ae
Sabasiva06 <sabasiva06@gmail.com>
2021-04-28 13:43:48 +0530

    class2 works

--- src/test/java/org/sample/A.java
+++ src/test/java/org/sample/A.java
@@ -18,6 +18,51 @@ public class A {
 		System.out.println("test2 in A");
 	}
 	
+	@Test
+	private void test7() {
+		System.out.println("test 7 in A");
+	}
+	
+	@Test
+	private void test8() {
+		System.out.println("test 8 in A");
+	}
+	
+	@Test
+	private void test9() {
+		System.out.println("test 9 in A");
+	}
+	
+	@Test
+	private void test10() {
+		System.out.println("test 10 in A");
+	}
+	
+	@Test
+	private void test11() {
+		System.out.println("test 11 in A");
+	}
+	
+	@Test
+	private void test12() {
+		System.out.println("test 12 in A");
+	}
+	@Test
+	private void test13() {
+		System.out.println("test 13 in A");
+	}
+	
+	@Test
+	private void test14() {
+		System.out.println("test 14 in A");
+	}
+	
+	@Test
+	private void test15() {
+		System.out.println("test 15 in A");
+	}
+	
+	
 	
 	
 
